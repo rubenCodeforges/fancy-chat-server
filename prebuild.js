1c8c5b09e87f451d0867cc53ce30c6d55fb5f962
codeforges <ruben@codeforges.com>
2020-07-06 21:07:35 +0200

    prebuild.js

---
+++ prebuild.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const replace = require('replace-in-file');
+
+const options = {
+
+    //Single file
+    files: 'client/main.*',
+
+    from: 'host:process.env.HOST',
+    to: 'host:' + process.env.HOST,
+};
+
+try {
+   replace.sync(options);
+}
+catch (error) {
+    console.error('Error occurred:', error);
+}
